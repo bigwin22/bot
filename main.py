15e3da69126521f0113909236eba8adedc648a08
bigwin22 <bigwin222@users.noreply.github.com>
2021-03-13 13:15:46 +0900

    디코봇

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import discord
 from discord.ext import commands
+import requests
+import json
 
 ##학교코드:7041189
 
@@ -17,14 +19,48 @@ async def on_ready():
     print(client.user.id)
     print("================")
 
+def getcode(name):
+    url = 'https://schoolmenukr.ml/code/api?q=' + name
+    response = requests.get(url)
+    school_infos = json.loads(response.text)
+    code = [info['code'] for info in school_infos['school_infos']]
+    val = ''
+    for a in code:
+        val += a
+    return val
+
+def gettype(name):
+    url = 'https://schoolmenukr.ml/code/api?q=' + name
+    response = requests.get(url)
+    school_infos = json.loads(response.text)
+    code = [info['estType'] for info in school_infos['school_infos']]
+    val = ''
+    for a in code:
+        val += a
+    return val
+
+def getfood(*arr, code, type):
+    url = 'https://schoolmenukr.ml/api/'+type+'/'+code+'?'
+    response = requests.get(url)
+    school_menu = json.loads(response.text)
+    test = [info['lunch'] for info in school_infos['menu']]
+    val = ''
+    for a in test:
+        val += a
+    print(val)
+
+
+
 # 봇이 특정 메세지를 받고 인식하는 코드
 @client.command()
-async def 급식(ctx,type,name,year,month,day):
+async def 급식(ctx, *val):
+    code = getcode(val[0])
+    typ = gettype(val[0])
+    #food = getfood(val,code,typ)
+    await ctx.send(code+' '+typ)
 
 
-    await ctx.send()
-
-async def 타이머(ctx, *text):
+'''async def 타이머(ctx, *text):'''
 
 
 client.run(token)
--- test.py
+++ test.py
@@ -5,9 +5,9 @@ import json
 url = 'https://schoolmenukr.ml/code/api?q=영서중학교'
 response = requests.get(url)
 school_infos = json.loads(response.text)
+school_codes = [info['code'] for info in school_infos['school_infos']]
 print(school_infos)
 
-url = 'https://schoolmenukr.ml/api/middle/B100001189?year=2019&month=10&date=21&allergy=hidden'
-response = requests.get(url)
-school_menu = json.loads(response.text)
-print(school_menu)
+
+
+
